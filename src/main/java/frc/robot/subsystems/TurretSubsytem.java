27a7e5bd990b6edb9a558fac0985245d5f95369d
Rafael Henritzi <rafaelhenritzi@gmail.com>
2025-12-26 22:11:22 -0300

    PoseEstimate for TurretAlign and Corrections

--- src/main/java/frc/robot/subsystems/TurretSubsytem.java
+++ src/main/java/frc/robot/subsystems/TurretSubsytem.java
@@ -2,30 +2,41 @@ package frc.robot.subsystems;
 
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.wpilibj.Filesystem;
+
+import java.io.File;
+
 import com.revrobotics.spark.SparkFlex;
 import frc.Java_Is_AllMight.Control.PIDConfig;
 import frc.Java_Is_AllMight.Motors.SparkConfigurator;
+import frc.robot.subsystems.SwerveSubsystem;
 
 public class TurretSubsytem {
     public SparkFlex shooterMotor;
     public SparkFlex turretMotor;
+    private SwerveSubsystem swerve;
 
     public TurretSubsytem() {
         PIDConfig motorPID = new PIDConfig(0.5, 0.0, 0.0, 0.0, 0.0);
         shooterMotor = SparkConfigurator.createSparkFlex(1, MotorType.kBrushless, motorPID, IdleMode.kCoast, 40); 
         turretMotor = SparkConfigurator.createSparkFlex(2, MotorType.kBrushless, motorPID, IdleMode.kBrake, 40);
+        swerve = new SwerveSubsystem(new File(Filesystem.getDeployDirectory(), "swerve"));
     }
 
     void shoot(){
 
     }
 
-    void alignTurret(){
+    void alignTurret(Pose2d pose){
+        pose = swerve.getPose();
 
+        //alinhar
+        //alinhar
+        //alinhar
+        //alinhar
     }
 
-    void getRobotPose(){
-
-    }
 
 }
